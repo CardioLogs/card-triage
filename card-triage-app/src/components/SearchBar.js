a556c1bf1f19969f0b1219d33fc9b0c88fbe892b
Miness <marianneg1512@gmail.com>
2021-06-13 17:50:43 +0200

    search input event

--- card-triage-app/src/components/SearchBar.js
+++ card-triage-app/src/components/SearchBar.js
@@ -1,22 +1,27 @@
 import "./SearchBar.css";
 
 function SearchBar() {
+
+    const SearchinputHandler = () => {
+        console.log("search")
+    }
+
   return (
     <div className="search-bar">
-      <div class="input-group rounded">
+      <div className="input-group rounded">
         <input
           type="search"
           className="form-control rounded"
           placeholder="Search"
           aria-label="Search"
           aria-describedby="search-addon"
+          onInput={SearchinputHandler}
         />
-        <span class="input-group-text border-0" id="search-addon">
-          <i class="bi bi-search"></i>
+        <span className="input-group-text border-0" id="search-addon">
+          <i className="bi bi-search"></i>
         </span>
       </div>
     </div>
-  );
-}
+  )}
 
 export default SearchBar;
